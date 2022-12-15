9feff03dac94eed3fff36bdac58374fea7a7c990
harshitkhardia <harshitkhardia@gmail.com>
2022-12-15 20:50:40 +0530

    Initial commit - spring intializer

---
+++ src/main/java/Trading/Journal/Services/ServletInitializer.java
@@ -0,0 +1,13 @@
+package Trading.Journal.Services;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JournalApplication.class);
+	}
+
+}
---
+++ src/test/java/Trading/Journal/Services/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package Trading.Journal.Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
